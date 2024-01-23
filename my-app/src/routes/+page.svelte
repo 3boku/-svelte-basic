b88e9b810a1deb324d491de5f40431b91afbda98
sambok23 <a24746440@gmail.com>
2024-01-24 01:21:41 +0900

    svelte kit learn

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    let name = 'kim'
+</script>
+
+<h1>{name}의 홈페이지임</h1>
+
+<style>
+    h1 {
+        font-size: 30px;
+    }
+</style>
---
+++ my-app/src/routes/about/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let data
+</script>
+
+<form method="POST" action="/어쩌구">
+    <input name="email">
+    <input name="password">
+    <button>Log in</button>
+</form>
+
+<h1>About임! {data.title}</h1>
